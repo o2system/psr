23512c1dcbee3665a5b7745dd7ed5bc4ed81aa89
Steeven Andrian <steevenz@live.com>
2016-09-08 16:43:44 +0700

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# psr
-PHP Framework Interop Group Standard Rules
+# PHP Standards Recommendations (PSR)
+
+O2System PSR it's build based on [PHP Framework Interop Group (PHP-FIG)](http://php-fig.org) standards recommendations and is added by some standards recommendations for O2System PHP Framework.
+This repository contains a collection of PHP interfaces based on the PSR-0 until the PSR-7.
+
+Installation
+------------
+The best way to install [O2System PSR](https://packagist.org/packages/o2system/psr) is to use [Composer](http://getcomposer.org)
+```
+composer require o2system/psr
+```
+
+Usage Example
+-------------
+```php
+namespace O2System\Gears;
+
+use O2System\Psr\Log\LoggerInterface;
+
+class Logger implements LoggerInterface
+{
+
+}
+```
+
+Ideas and Suggestions
+---------------------
+Please kindly mail us at [o2system.framework@gmail.com][mailto:o2system.framework@gmail.com].
+
+Bugs and Issues
+---------------
+Please kindly submit your [issues at Github](http://github.com/o2system/psr/issues) so we can track all the issues along development.
+
+System Requirements
+-------------------
+- PHP 5.4+
+- [Composer](http://getcomposer.org)
+
+Credits
+-------
+* Founder and Lead Projects: [Steeven Andrian Salim (steevenz.com)](http://steevenz.com)
+* Github Pages Designer and Writer: [Teguh Rianto](http://teguhrianto.tk)
---
+++ src/Cache/CacheItemInterface.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * O2System
+ *
+ * An open source application development framework for PHP 5.4.0 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, O2System Framework Developer Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package        O2System\Core
+ * @author         O2System Framework Developer Team
+ * @copyright      Copyright (c) 2005 - 2014, O2System PHP Framework
+ * @license        http://www.o2system.io/license.html
+ * @license        http://opensource.org/licenses/MIT	MIT License
+ * @link           http://www.o2system.io
+ * @since          Version 2.0
+ * @filesource
+ */
+// ------------------------------------------------------------------------
+
+namespace O2System\Psr\Cache;
+
+// ------------------------------------------------------------------------
+
+/**
+ * CacheItemInterface defines an interface for interacting with objects inside a cache.
+ */
+interface CacheItemInterface
+{
+    /**
+     * Returns the key for the current cache item.
+     *
+     * The key is loaded by the Implementing Library, but should be available to
+     * the higher level callers when needed.
+     *
+     * @return string
+     *   The key string for this cache item.
+     */
+    public function getKey();
+
+    /**
+     * Retrieves the value of the item from the cache associated with this object's key.
+     *
+     * The value returned must be identical to the value originally stored by set().
+     *
+     * If isHit() returns false, this method MUST return null. Note that null
+     * is a legitimate cached value, so the isHit() method SHOULD be used to
+     * differentiate between "null value was found" and "no value was found."
+     *
+     * @return mixed
+     *   The value corresponding to this cache item's key, or null if not found.
+     */
+    public function get();
+
+    /**
+     * Confirms if the cache item lookup resulted in a cache hit.
+     *
+     * Note: This method MUST NOT have a race condition between calling isHit()
+     * and calling get().
+     *
+     * @return bool
+     *   True if the request resulted in a cache hit. False otherwise.
+     */
+    public function isHit();
+
+    /**
+     * Sets the value represented by this cache item.
+     *
+     * The $value argument may be any item that can be serialized by PHP,
+     * although the method of serialization is left up to the Implementing
+     * Library.
+     *
+     * @param mixed $value
+     *   The serializable value to be stored.
+     *
+     * @return static
+     *   The invoked object.
+     */
+    public function set($value);
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param \DateTimeInterface|null $expiration
+     *   The point in time after which the item MUST be considered expired.
+     *   If null is passed explicitly, a default value MAY be used. If none is set,
+     *   the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAt($expiration);
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param int|\DateInterval|null $time
+     *   The period of time from the present after which the item MUST be considered
+     *   expired. An integer parameter is understood to be the time in seconds until
+     *   expiration. If null is passed explicitly, a default value MAY be used.
+     *   If none is set, the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAfter($time);
+
+}
